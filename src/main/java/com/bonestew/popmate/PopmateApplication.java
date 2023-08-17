c8d5299d188bb2580965b6a95bbb6b22c9008487
sa46lll <azure394@gmail.com>
2023-08-17 20:52:15 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/bonestew/popmate/PopmateApplication.java
@@ -0,0 +1,13 @@
+package com.bonestew.popmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bonestew/popmate/PopmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonestew.popmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
