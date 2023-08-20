3ff60499e4689f66c6aab080e3630d7cc62126d1
sa46lll <azure394@gmail.com>
2023-08-21 01:18:28 +0900

    feat: custom exception 클래스 추가

---
+++ src/main/java/com/bonestew/popmate/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.bonestew.popmate.exception;
+
+public class NotFoundException extends PopMateException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
