68f79042ce7e73802160036f069afc9ba8cdbc85
sa46lll <azure394@gmail.com>
2023-08-21 01:21:18 +0900

    feat: 팝업스토어 단일 조회 틀 구현

---
+++ src/main/java/com/bonestew/popmate/popupstore/domain/PopupStore.java
@@ -0,0 +1,17 @@
+package com.bonestew.popmate.popupstore.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PopupStore {
+
+    Long popupStoreId;
+    String name;
+    String description;
+}
