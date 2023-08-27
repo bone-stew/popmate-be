9de8504adeee402d340cc0b774e24830f672927a
jae-ryong <frogs6225@naver.com>
2023-08-27 13:21:26 +0900

    feat: 스프링 시큐리티에서 유저 정보 가져오기

--- src/main/java/com/bonestew/popmate/auth/presentation/OauthController.java
+++ src/main/java/com/bonestew/popmate/auth/presentation/OauthController.java
@@ -4,11 +4,16 @@ import com.bonestew.popmate.auth.application.OauthService;
 import com.bonestew.popmate.auth.domain.OauthUser;
 import com.bonestew.popmate.security.application.AuthenticationService;
 import com.bonestew.popmate.security.application.JwtAuthenticationResponse;
+import com.bonestew.popmate.security.application.JwtService;
+import com.bonestew.popmate.security.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OauthController {
 
     private final OauthService oauthService;
-    private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
     // 여기서는 카카오 쪽
     @PostMapping("/oauth/{code}")
     @ResponseBody
@@ -36,7 +41,17 @@ public class OauthController {
     @ResponseBody
     public ResponseEntity<JwtAuthenticationResponse> googleOauth(@RequestBody OauthUser oauthUser){
         JwtAuthenticationResponse jwtToken = oauthService.loginGoogleOauthService(oauthUser);
-
         return ResponseEntity.ok(jwtToken);
+    }
+
+    @GetMapping("/resource")
+    public ResponseEntity<User> sayHello(@RequestHeader("Authorization") String accessToken) {
+        // token 받아서 이렇게 User 정보 가져오면 됩니다
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(user.toString());
+        System.out.println(user.getUserId());
+        System.out.println(user.getName());
+        System.out.println(user.getProvider());
+        return ResponseEntity.ok(user);
     }
 }
