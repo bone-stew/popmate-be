096cb4580e0286b2e73f88df09b697e0483198dd
Sangwon <40656716+swc617@users.noreply.github.com>
2023-08-29 11:16:27 +0900

    feat: AWS S3 세팅 (#36)
    * feat: AWS S3 세팅
    
    * fix: 테스트 값 설정
    
    * fix: 테스트 값 수정

--- src/main/java/com/bonestew/popmate/popupstore/presentation/PopupStoreController.java
+++ src/main/java/com/bonestew/popmate/popupstore/presentation/PopupStoreController.java
@@ -1,6 +1,17 @@
 package com.bonestew.popmate.popupstore.presentation;
 
 import com.bonestew.popmate.dto.ApiResponse;
+import com.bonestew.popmate.exception.enums.ResultCode;
+import com.bonestew.popmate.popupstore.config.FolderType;
+import com.bonestew.popmate.popupstore.config.service.AwsFileService;
+import com.bonestew.popmate.popupstore.presentation.dto.PopupStoreResponse;
+import com.bonestew.popmate.popupstore.domain.PopupStore;
+import com.bonestew.popmate.popupstore.application.PopupStoreService;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import com.bonestew.popmate.popupstore.domain.Banner;
 import com.bonestew.popmate.popupstore.domain.PopupStore;
 import com.bonestew.popmate.popupstore.application.PopupStoreService;
@@ -19,7 +30,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,6 +40,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PopupStoreController {
 
     private final PopupStoreService popupStoreService;
+    private final AwsFileService awsFileService;
 
     @GetMapping
     public ApiResponse<PopupStoresResponse> getPopupStoreList(
@@ -60,4 +74,14 @@ public class PopupStoreController {
         List<PopupStoreItem> popupStoreItemList = popupStoreService.getPopupStoreGoods(popupStoreId);
         return ApiResponse.success(PopupStoreItemsResponse.of(popupStore, popupStoreItemList));
     }
+
+    @PostMapping("/banner")
+    public ApiResponse<String> addBanner(@RequestParam MultipartFile multipartFile) {
+        Optional<String> bannerImgUrl = awsFileService.upload(multipartFile, FolderType.BANNERS);
+        if (bannerImgUrl.isPresent()) {
+            return ApiResponse.success(bannerImgUrl.get());
+        }
+        return ApiResponse.failure(ResultCode.FAILURE, "파일 업로드 에러");
+    }
 }
+
