68c54606b4e9cbc744762d8666271e006492364c
jae-ryong <frogs6225@naver.com>
2023-09-11 10:19:03 +0900

    feat : 스프링 시큐리티 예외처리 추가

--- src/main/java/com/bonestew/popmate/auth/presentation/OauthController.java
+++ src/main/java/com/bonestew/popmate/auth/presentation/OauthController.java
@@ -5,7 +5,6 @@ import com.bonestew.popmate.auth.domain.JwtAuthenticationResponse;
 import com.bonestew.popmate.auth.presentation.dto.GoogleLoginRequest;
 import com.bonestew.popmate.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
