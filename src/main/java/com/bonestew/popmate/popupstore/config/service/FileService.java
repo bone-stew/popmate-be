27e61d9abee3c90f2b3fb3a46d21b82c138005c0
sa46lll <azure394@gmail.com>
2023-09-12 17:17:42 +0900

    feat: QR코드 생성 후 S3 이미지 저장 구현

--- src/main/java/com/bonestew/popmate/popupstore/config/service/FileService.java
+++ src/main/java/com/bonestew/popmate/popupstore/config/service/FileService.java
@@ -1,10 +1,13 @@
 package com.bonestew.popmate.popupstore.config.service;
 
 import com.bonestew.popmate.popupstore.config.FolderType;
+import java.io.InputStream;
 import java.util.Optional;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
     Optional<String> upload(MultipartFile multipartFile, FolderType folderType);
+
+    String uploadInputStream(InputStream inputStream, String directory);
 }
--- src/main/java/com/bonestew/popmate/reservation/application/ReservationEventService.java
+++ src/main/java/com/bonestew/popmate/reservation/application/ReservationEventService.java
@@ -1,5 +1,9 @@
 package com.bonestew.popmate.reservation.application;
 
+import static com.bonestew.popmate.popupstore.config.FolderType.RESERVATIONS;
+
+import com.bonestew.popmate.popupstore.config.service.FileService;
+import com.bonestew.popmate.user.application.UserService;
 import com.bonestew.popmate.user.domain.User;
 import com.bonestew.popmate.reservation.application.dto.ReservationRequest;
 import com.bonestew.popmate.reservation.domain.Reservation;
@@ -8,6 +12,7 @@ import com.bonestew.popmate.reservation.exception.AlreadyReservedException;
 import com.bonestew.popmate.reservation.exception.ReservationNotFoundException;
 import com.bonestew.popmate.reservation.persistence.ReservationDao;
 import com.bonestew.popmate.reservation.persistence.UserReservationDao;
+import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,12 +25,15 @@ public class ReservationEventService {
 
     private final ReservationDao reservationDao;
     private final UserReservationDao userReservationDao;
+    private final UserService userService;
+    private final QrService qrService;
+    private final FileService fileService;
 
     @Transactional
     public void reserve(final Long reservationId, final Long userId, final ReservationRequest reservationRequest) {
         Reservation reservation = reservationDao.findById(reservationId)
             .orElseThrow(() -> new ReservationNotFoundException(reservationId));
-        User user = new User(userId, null, null, null, null, null, null); // 임시 (userDao 필요)
+        User user = userService.getUserById(userId);
 
         // TODO: 사용자가 예약 가능한 위치인지 확인
 
@@ -37,13 +45,20 @@ public class ReservationEventService {
         reservation.increaseCurrentGuestCount(reservationRequest.guestCount());
         reservationDao.updateCurrentGuestCount(reservation);
 
-        // TODO: QR 코드 생성
-        String qrImgUrl = "Sample URL";
+        String reservationQrCode = generateReservationQrCode(user, reservation);
 
         userReservationDao.save(UserReservation.of(
-            user, reservation, qrImgUrl, reservationRequest.guestCount()
+            user, reservation, reservationQrCode, reservationRequest.guestCount()
         ));
 
         log.info("Reservation successful for user ID: {}, reservation ID: {}", user.getUserId(), reservationId);
+    }
+
+    private String generateReservationQrCode(User user, Reservation reservation) {
+        InputStream inputStream = qrService.generateQRCode(user.getUserId(), reservation.getReservationId());
+        String directory = String.format(RESERVATIONS.getFolderName(),
+            reservation.getPopupStore().getPopupStoreId(), reservation.getReservationId());
+
+        return fileService.uploadInputStream(inputStream, directory);
     }
 }
