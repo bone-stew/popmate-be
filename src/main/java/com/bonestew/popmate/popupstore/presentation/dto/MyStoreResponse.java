4263570ea59fdad2b14f92461e8f5eb05835649d
kosa-kww <dyffh1031@gmail.com>
2023-09-22 16:46:09 +0900

    feat: 사이드바 목록 api 예약, 구매 여부 추가

--- src/main/java/com/bonestew/popmate/popupstore/presentation/dto/MyStoreResponse.java
+++ src/main/java/com/bonestew/popmate/popupstore/presentation/dto/MyStoreResponse.java
@@ -2,8 +2,8 @@ package com.bonestew.popmate.popupstore.presentation.dto;
 
 import com.bonestew.popmate.popupstore.domain.PopupStore;
 
-public record MyStoreResponse(Long popupStoreId, String title) {
+public record MyStoreResponse(Long popupStoreId, String title, Boolean reservationEnabled, Boolean salesEnabled ) {
     public static MyStoreResponse from(PopupStore popupStore) {
-        return new MyStoreResponse(popupStore.getPopupStoreId(), popupStore.getTitle());
+        return new MyStoreResponse(popupStore.getPopupStoreId(), popupStore.getTitle(), popupStore.getReservationEnabled(), popupStore.getSalesEnabled());
     }
 }
