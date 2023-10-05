fd1aa0d8cc9ac83a46c7c8e5bbdb259063635168
sa46lll <azure394@gmail.com>
2023-10-06 00:17:33 +0900

    feat: 선착순 예약 동시성 해결을 위한 분산락 구현
    issue: #204

--- src/main/java/com/bonestew/popmate/PopmateApplication.java
+++ src/main/java/com/bonestew/popmate/PopmateApplication.java
@@ -3,11 +3,13 @@ package com.bonestew.popmate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.bonestew")
 public class PopmateApplication {
 
     public static void main(String[] args) {
