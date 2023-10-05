529e7a61106a198b5d28022b93556bec6849182f
Sangwon <sangcho617@gmail.com>
2023-10-06 00:56:14 +0900

    feat: 팝업스토어 홈 테스트 코드

--- src/test/java/com/bonestew/popmate/popupstore/presentation/PopupStoreControllerTest.java
+++ src/test/java/com/bonestew/popmate/popupstore/presentation/PopupStoreControllerTest.java
@@ -11,8 +11,11 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.bonestew.popmate.auth.domain.PopmateUser;
+import com.bonestew.popmate.popupstore.domain.Banner;
 import com.bonestew.popmate.popupstore.domain.Category;
 import com.bonestew.popmate.popupstore.domain.CategoryType;
+import com.bonestew.popmate.user.domain.Role;
 import com.bonestew.popmate.user.domain.User;
 import com.bonestew.popmate.chat.domain.ChatRoom;
 import com.bonestew.popmate.exception.enums.ResultCode;
@@ -27,6 +30,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.swing.Popup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +60,52 @@ class PopupStoreControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private PopupStore popupStore;
+    private List<PopupStore> popupStoreList;
+    private List<Banner> bannerList;
+    private User user;
+    private PopmateUser popmateUser;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setUserId(1L);
+        user.setNickname("test");
+        user.setRole(Role.ROLE_USER);
+        popmateUser = PopmateUser.from(user);
+        popupStore =  new PopupStore(
+                1L,
+                new User(),
+                new Department(1L, "department", "placeDescription", 12.1, 12.1, LocalDateTime.of(2023, 10, 4, 9, 0),
+                               LocalDateTime.of(2024, 1, 1, 17, 0), LocalDateTime.now() ),
+                new ChatRoom("1", "chatroom", LocalDateTime.now()),
+                new Category(1L, CategoryType.POPUP_STORE),
+                "팝업스토어",
+                "주최자",
+                "상세 주소",
+                "설명",
+                "이벤트",
+                "https://example.com/banner.jpg",
+                0,
+                50,
+                true,
+                true,
+                30,
+                5,
+                10,
+                LocalDateTime.of(2023, 10, 4, 10, 0),
+                LocalDateTime.of(2024, 1, 1, 9, 0),
+                LocalDateTime.of(2023, 10, 4, 9, 0),
+                LocalDateTime.of(2024, 1, 1, 17, 0),
+                0L,
+                LocalDateTime.now(),
+                0);
+        bannerList = List.of(
+                new Banner(1L, "img_url", popupStore, LocalDateTime.now())
+        );
+        popupStoreList = List.of( popupStore);
+
+    }
 
 //    @Disabled
     @Test
@@ -69,36 +120,6 @@ class PopupStoreControllerTest {
                         null,
                         null
                 );
-        List<PopupStore> popupStoreList = List.of(
-                new PopupStore(
-                        1L,
-                        new User(),
-                        new Department(1L, "department", "placeDescription", 12.1, 12.1, LocalDateTime.of(2023, 10, 4, 9, 0),
-                                       LocalDateTime.of(2024, 1, 1, 17, 0), LocalDateTime.now() ),
-                        new ChatRoom("1", "chatroom", LocalDateTime.now()),
-                        new Category(1L, CategoryType.POPUP_STORE),
-                        "팝업스토어",
-                        "주최자",
-                        "상세 주소",
-                        "설명",
-                        "이벤트",
-                        "https://example.com/banner.jpg",
-                        0,
-                        50,
-                        true,
-                        true,
-                        30,
-                        5,
-                        10,
-                        LocalDateTime.of(2023, 10, 4, 10, 0),
-                        LocalDateTime.of(2024, 1, 1, 9, 0),
-                        LocalDateTime.of(2023, 10, 4, 9, 0),
-                        LocalDateTime.of(2024, 1, 1, 17, 0),
-                        0L,
-                        LocalDateTime.now(),
-                        0)
-
-        );
 
 
         // When
@@ -128,6 +149,8 @@ class PopupStoreControllerTest {
                                 fieldWithPath("rowsToGet").description("반환할 데이터 수")
                         ),
                         responseFields(
+
+
                                 fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
                                 fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
                                 fieldWithPath("data.popupStores[].popupStoreId").type(JsonFieldType.NUMBER).description("예약 가능 여부"),
@@ -142,8 +165,62 @@ class PopupStoreControllerTest {
                                 fieldWithPath("data.popupStores[].createdAt").type(JsonFieldType.STRING).description("생성날짜"),
                                 fieldWithPath("data.popupStores[].total").type(JsonFieldType.NUMBER).description("조회수")
 
+
+
+
                         )
                 ));
+    }
+
+    @Test
+    void 팝업스토어_홈을_조회한다() throws Exception {
+        given(popupStoreService.getBanners()).willReturn(bannerList);
+        given(popupStoreService.getPopupStoresVisitedBy(user.getUserId())).willReturn(popupStoreList);
+        given(popupStoreService.getPopupStoresRecommend()).willReturn(popupStoreList);
+        given(popupStoreService.getPopupStoresEndingSoon()).willReturn(popupStoreList);
+
+
+        ResultActions result = mockMvc.perform(
+                get("/api/v1/popup-stores/home")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(popmateUser))
+        );
+        result
+                .andExpect(status().isOk())
+                .andDo(customDocument(
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data.banners[].bannerId").type(JsonFieldType.NUMBER).description("예약 가능 여부"),
+                                fieldWithPath("data.banners[].imgUrl").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.banners[].popupStoreId").type(JsonFieldType.NUMBER).description("예약 가능 여부"),
+
+
+                                fieldWithPath("data.popupStoresVisitedBy[].popupStoreId").type(JsonFieldType.NUMBER).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresVisitedBy[].title").type(JsonFieldType.STRING).description("팝업스토어 이름"),
+                                fieldWithPath("data.popupStoresVisitedBy[].bannerImgUrl").type(JsonFieldType.STRING).description("시작 시간"),
+
+                                fieldWithPath("data.popupStoresRecommend[].popupStoreId").type(JsonFieldType.NUMBER).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].title").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].categoryName").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].openDate").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].closeDate").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].placeDetail").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].bannerImgUrl").type(JsonFieldType.STRING).description("예약 가능 여부"),
+                                fieldWithPath("data.popupStoresRecommend[].organizer").type(JsonFieldType.STRING).description("예약 가능 여부"),
+
+                                fieldWithPath("data.popupStoresEndingSoon[].popupStoreId").type(JsonFieldType.NUMBER).description("종료 시간"),
+                                fieldWithPath("data.popupStoresEndingSoon[].title").type(JsonFieldType.STRING).description("상세 주소"),
+                                fieldWithPath("data.popupStoresEndingSoon[].categoryName").type(JsonFieldType.STRING).description("배너 이미지 URL"),
+                                fieldWithPath("data.popupStoresEndingSoon[].openDate").type(JsonFieldType.STRING).description("주최자"),
+                                fieldWithPath("data.popupStoresEndingSoon[].closeDate").type(JsonFieldType.STRING).description("백화점 이름"),
+                                fieldWithPath("data.popupStoresEndingSoon[].placeDetail").type(JsonFieldType.STRING).description("카테고리 명"),
+                                fieldWithPath("data.popupStoresEndingSoon[].bannerImgUrl").type(JsonFieldType.STRING).description("생성날짜"),
+                                fieldWithPath("data.popupStoresEndingSoon[].organizer").type(JsonFieldType.STRING).description("조회수")
+
+                        )
+                ));
+
     }
 
 
